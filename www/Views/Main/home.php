d083c2454fae2d30d72f62dc8ed40e77a8ae1688
Pulsy BACKEKOLO <pulsycaleb@gmail.com>
2024-04-16 18:58:42 +0200

    ajout du premier view

--- www/Views/Main/home.php
+++ www/Views/Main/home.php
@@ -1,5 +1,25 @@
-<h2>Gérez le site Web de votre club</h2>
-<h3>Customize your club site, add player profiles,
-match schedules, and latest club news easily.</h3>
-
-<button>Demarrer</button>
+<div class="home">
+    <div class="containt">
+    <section>
+        <div class="logo">
+            <img src="Asset/images/logo.svg" alt="image">
+            <h2>ClubWebsiteCMS</h2>
+        </div>
+        <h1>
+        Gérez le site Web de votre club
+        </h1>
+        <p>Personnalisez le site de votre club, ajoutez des profils de joueurs,
+        les horaires des matchs et les dernières nouvelles du club facilement.</p>
+        <div class="next">
+            <div class="button">
+                Commencez
+            </div>
+        </div>
+    </section>
+    <div class="left">
+        <div class="image">
+            <img src="Asset/images/foot.jpeg" alt="image">
+        </div>
+    </div>
+    </div>
+</div>
